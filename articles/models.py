c7662c9408637f739d6e223994223090fd0a1847
Sharma-Hrishabh <30hrs1999@gmail.com>
2019-01-03 18:06:48 +0530

    comment model added

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Articles(models.Model):
@@ -17,3 +18,18 @@ class Articles(models.Model):
 
     def snippet(self):
         return self.body[:100] + '...'
+
+
+class Comment(models.Model):
+    article = models.ForeignKey('Articles', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
